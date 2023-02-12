f0ac8956c8103bb3b31eeeafd364e7ea85dc1bc8
caiyb <caiyb@sh.woowis.com>
2023-02-12 17:30:48 +0800

    room-item

---
+++ src/components/room-item/index.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+import ItemWrapper from './style'
+import { Rating } from '@mui/material'
+
+const RoomItem = memo((props) => {
+  const { roomData } = props
+  return (
+    <ItemWrapper verifyColor={ roomData.verify_info.textColor || '#39576a'}>
+      <div className='inner'>
+        <div className='cover'>
+          <img src={roomData.picture_url} alt=''/>
+        </div>
+        <div className='desc'>
+          { roomData.verify_info.messages.join(' · ')}
+        </div>
+        <div className='name'>
+          { roomData.name }
+        </div>
+        <div className='price'>
+          ￥{roomData.price}/晚
+        </div>
+        <div className='bottom'>
+          <Rating
+            value={ roomData.star_rating ?? 5 }
+            precision={0.1}
+            readOnly
+            sx={{ fontSize: "12px", color: "#00848A", marginRight: "-1px" }}
+          />
+          <span className='count'>{roomData.reviews_count}</span>
+          {
+            roomData.bottom_info && <span className='extra'>·{roomData.bottom_info?.content}</span>
+          }
+        </div>
+      </div>
+    </ItemWrapper>
+  )
+})
+
+RoomItem.propTypes = {
+  roomData: PropTypes.object
+}
+
+export default RoomItem
---
+++ src/components/room-item/style.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+
+const ItemWrapper = styled.div`
+    width: 25%;
+    padding: 0 8px 12px;
+    .cover {
+        position: relative;
+        padding: 66.66% 0 0;
+        border-radius: 3px;
+        overflow: hidden;
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    .desc {
+        margin: 10px 0 5px;
+        font-size: 12px;
+        font-weight: 700;
+        color: ${props => props.verifyColor};
+    }
+
+  .name {
+    font-size: 16px;
+    font-weight: 700;
+
+    overflow: hidden;  
+    text-overflow: ellipsis; 
+    display: -webkit-box; 
+    -webkit-line-clamp: 2; 
+    -webkit-box-orient: vertical;
+  }
+
+  .price {
+    margin: 8px 0;
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    font-weight: 600;
+    color: ${props => props.theme.text.primary};
+
+    .count {
+      margin: 0 2px 0 4px;
+    }
+
+    .MuiRating-decimal {
+        margin-right: -2px;
+    }
+  }
+`
+
+export default ItemWrapper
