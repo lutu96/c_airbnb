c59f4e0fb9bf61da2d59e95536cd6c73abf64dac
caiyb <caiyb@sh.woowis.com>
2023-02-12 17:32:03 +0800

    首页图片

---
+++ src/views/home/c-cpns/home-banner/index.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react'
+import BannerWrapper from './style'
+// import bgBanner from '@/assets/img/cover_01.jpeg'
+const HomeBanner = memo(() => {
+  return (
+    <BannerWrapper>
+        {/* <img src={bgBanner} /> */}
+    </BannerWrapper>
+  )
+})
+
+export default HomeBanner
---
+++ src/views/home/c-cpns/home-banner/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const BannerWrapper = styled.div`
+    height: 529px;
+    /* img {
+        width: 100%;
+    } */
+    background: url(${require('@/assets/img/cover_01.jpeg')}) center/cover;
+    
+`
+
+export default BannerWrapper
