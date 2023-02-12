39de22f088b1f502131d054bbd47246dfd307772
caiyb <caiyb@sh.woowis.com>
2023-02-12 17:32:42 +0800

    相关配置

--- craco.config.js
+++ craco.config.js
@@ -13,7 +13,8 @@ module.exports = {
     alias: {
         '@': resolve('src'),
         'components': resolve('src/components'),
-        'utils': resolve('src/utils')
+        'utils': resolve('src/utils'),
+        // '@mui/styled-engine': '@mui/styled-engine-sc'
     }
   }
 }
